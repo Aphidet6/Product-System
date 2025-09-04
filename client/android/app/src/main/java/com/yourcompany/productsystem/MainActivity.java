e558e5f8195d5480c84a61362f141eede8a66909
Earth <earthhh001@gmail.com>
2025-09-04 13:28:44 +0700

    1

---
+++ client/android/app/src/main/java/com/yourcompany/productsystem/MainActivity.java
@@ -0,0 +1,5 @@
+package com.yourcompany.productsystem;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ client/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.yourcompany.productsystem',
+  appName: 'product-system',
+  webDir: 'dist'
+};
+
+export default config;
